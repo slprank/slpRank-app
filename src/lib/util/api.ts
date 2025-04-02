5cc102f7cd4371519a5a8d9e8f2bfdefe357ae7d
Tony <magrimes+new_github@mtu.edu>
2025-04-02 11:13:07 -0700

    Use updated slippi API

--- src/lib/util/api.ts
+++ src/lib/util/api.ts
@@ -57,8 +57,63 @@ export async function fetchSlippiUser(connectCode: string): Promise<Player | und
 	}
 
 	try {
-		let res = await fetch('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
-			method: 'POST',
+		let res = await axios.post('https://gql-gateway-dot-slippi.uc.r.appspot.com/graphql', {
+			operationName: 'AccountManagementPageQuery',
+			variables: {
+				cc: connectCode,
+				uid: connectCode
+			},
+			query: `fragment profileFieldsV2 on NetplayProfileV2 {
+				id
+				ratingOrdinal
+				ratingUpdateCount
+				wins
+				losses
+				dailyGlobalPlacement
+				dailyRegionalPlacement
+				continent
+				characters {
+					character
+					gameCount
+					__typename
+				}
+				__typename
+			}
+
+			fragment userProfilePage on User {
+				fbUid
+				displayName
+				connectCode {
+					code
+					__typename
+				}
+				status
+				activeSubscription {
+					level
+					hasGiftSub
+					__typename
+				}
+				rankedNetplayProfile {
+					...profileFieldsV2
+					__typename
+				}
+				__typename
+			}
+
+			query AccountManagementPageQuery($cc: String!, $uid: String!) {
+				getUser(fbUid: $uid) {
+					...userProfilePage
+					__typename
+				}
+				getConnectCode(code: $cc) {
+					user {
+						...userProfilePage
+						__typename
+					}
+					__typename
+				}
+			}`
+		}, {
 			headers: {
 				'Content-Type': 'application/json',
 				Accept: '*/*',
@@ -70,67 +125,57 @@ export async function fetchSlippiUser(connectCode: string): Promise<Player | und
 					'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15',
 				Connection: 'keep-alive',
 				Referer: 'https://slippi.gg/',
-				'Content-Length': '838',
-				Priority: 'u=3, i',
 				'apollographql-client-name': 'slippi-web'
-			},
-			body: JSON.stringify({
-				operationName: 'AccountManagementPageQuery',
-				variables: {
-					cc: `${connectCode}`,
-					uid: `${connectCode}`
-				},
-				query:
-					'fragment userProfilePage on User {\n  fbUid\n  displayName\n  connectCode {\n    code\n    __typename\n  }\n  status\n  activeSubscription {\n    level\n    hasGiftSub\n    __typename\n  }\n  rankedNetplayProfile {\n    id\n    ratingOrdinal\n    ratingUpdateCount\n    wins\n    losses\n    dailyGlobalPlacement\n    dailyRegionalPlacement\n    continent\n    characters {\n      id\n      character\n      gameCount\n      __typename\n    }\n    __typename\n  }\n  __typename\n}\n\nquery AccountManagementPageQuery($cc: String!, $uid: String!) {\n  getUser(fbUid: $uid) {\n    ...userProfilePage\n    __typename\n  }\n  getConnectCode(code: $cc) {\n    user {\n      ...userProfilePage\n      __typename\n    }\n    __typename\n  }\n}\n'
-			})
+			}
 		});
 
-		let resUnpacked = await res.json();
-		let player: Player = resUnpacked?.data?.getConnectCode?.user;
-		if (!res) return;
+		let player = res.data?.data?.getConnectCode?.user;
+		if (!player) return;
+
+		const profile = player.rankedNetplayProfile;
+		const characters = profile.characters
+			.sort((a: any, b: any) => b.gameCount - a.gameCount)
+			.slice(0, 3)
+			.map((c: any): Character => ({
+				character: c.character,
+				characterName: c.character,
+				gameCount: c.gameCount,
+				icon: `${c.character.replace(/\s+/g, '_')}.png`,
+				characterId: -1,
+				characterColorId: -1
+			}));
 
 		return {
 			connectCode: connectCode,
 			displayName: player.displayName,
-			totalGames: player.rankedNetplayProfile.characters
+			character: characters[0],
+			totalGames: profile.characters
 				.map((c: any) => c.gameCount)
 				.reduce((a: number, b: number) => a + b, 0),
 			rankedNetplayProfile: {
-				...player,
+				...profile,
 				continentInitials:
-					player.rankedNetplayProfile.continent?.split('_').length == 2
-						? player.rankedNetplayProfile.continent
+					profile.continent?.split('_').length == 2
+						? profile.continent
 							.split('_')
 							.map((c: string) => c[0])
 							.join('')
-						: player.rankedNetplayProfile.continent?.substring(0, 2) ?? '',
-				characters: player?.rankedNetplayProfile?.characters
-					.sort((a: Character, b: Character) => b.gameCount - a.gameCount)
-					.slice(0, 3)
-					.map(
-						(c: Character): Character => ({
-							character: c.character,
-							gameCount: c.gameCount,
-							icon: `${c.character.replace(/\s+/g, '_')}.png`,
-							characterId: -1,
-							characterColorId: -1,
-							characterName: c.characterName?.toUpperCase() ?? ''
-						})
-				) as unknown as Character[],
-				dailyGlobalPlacement: player.rankedNetplayProfile.dailyGlobalPlacement,
-				dailyRegionalPlacement: player.rankedNetplayProfile.dailyRegionalPlacement,
-				wins: player.rankedNetplayProfile.wins ?? 0,
-				losses: player.rankedNetplayProfile.losses ?? 0,
-				ratingOrdinal: player.rankedNetplayProfile.ratingOrdinal,
+						: profile.continent?.substring(0, 2) ?? '',
+				characters,
+				dailyGlobalPlacement: profile.dailyGlobalPlacement,
+				dailyRegionalPlacement: profile.dailyRegionalPlacement,
+				wins: profile.wins ?? 0,
+				losses: profile.losses ?? 0,
+				ratingOrdinal: profile.ratingOrdinal,
 				rank: getPlayerRank(
-					player.rankedNetplayProfile.ratingOrdinal,
-					player.rankedNetplayProfile.dailyRegionalPlacement,
-					player.rankedNetplayProfile.dailyGlobalPlacement
+					profile.ratingOrdinal,
+					profile.dailyRegionalPlacement,
+					profile.dailyGlobalPlacement
 				)
 			},
 			stats: undefined
 		};
 	} catch (error) {
-		console.log(error);
+		console.log('Error fetching player data:', error);
 	}
 }
